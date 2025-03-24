0d907c2d5f238b3b739c1ee5952d75600f75e66f
MiguellBorgess <miguelpereiraborges@hotmail.com>
2025-03-24 10:10:41 -0300

    Mudancas nas polices glue_access_role

--- terraform/glue_access_role.tf
+++ terraform/glue_access_role.tf
@@ -15,9 +15,9 @@ resource "aws_iam_role" "glue_access_role" {
   })
 }
 
-resource "aws_iam_policy" "glue_access_policy" {
-  name        = "GlueAccessPolicy"
-  description = "Policy to allow Glue access"
+resource "aws_iam_policy" "glue_service_role_policy" {
+  name        = "GlueServiceRolePolicy"
+  description = "Policy for Glue Service Role"
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -25,18 +25,82 @@ resource "aws_iam_policy" "glue_access_policy" {
       {
         Effect = "Allow",
         Action = [
-          "glue:GetTable",
-          "glue:GetTables",
-          "glue:GetDatabase",
-          "glue:GetDatabases",
-          "glue:CreateTable",
-          "glue:UpdateTable",
-          "s3:GetObject",
-          "s3:PutObject",
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "s3:ListBucket"  # Permissão ampla adicionada
+          "glue:*",
+          "s3:GetBucketLocation",
+          "s3:ListBucket",
+          "s3:ListAllMyBuckets",
+          "s3:GetBucketAcl",
+          "ec2:DescribeVpcEndpoints",
+          "ec2:DescribeRouteTables",
+          "ec2:CreateNetworkInterface",
+          "ec2:DeleteNetworkInterface",
+          "ec2:DescribeNetworkInterfaces",
+          "ec2:DescribeSecurityGroups",
+          "ec2:DescribeSubnets",
+          "ec2:DescribeVpcAttribute",
+          "iam:ListRolePolicies",
+          "iam:GetRole",
+          "iam:GetRolePolicy",
+          "cloudwatch:PutMetricData"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["s3:CreateBucket"],
+        Resource = "arn:aws:s3:::aws-glue-*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["s3:GetObject", "s3:PutObject", "s3:DeleteObject"],
+        Resource = [
+          "arn:aws:s3:::aws-glue-*/*",
+          "arn:aws:s3:::*/*aws-glue-*/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = ["s3:GetObject"],
+        Resource = [
+          "arn:aws:s3:::crawler-public*",
+          "arn:aws:s3:::aws-glue-*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
+        Resource = "arn:aws:logs:*:*:*:/aws-glue/*"
+      },
+      {
+        Effect = "Allow",
+        Action = ["ec2:CreateTags", "ec2:DeleteTags"],
+        Condition = {
+          "ForAllValues:StringEquals": {
+            "aws:TagKeys": ["aws-glue-service-resource"]
+          }
+        },
+        Resource = [
+          "arn:aws:ec2:*:*:network-interface/*",
+          "arn:aws:ec2:*:*:security-group/*",
+          "arn:aws:ec2:*:*:instance/*"
+        ]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "amazon_s3_full_access_policy" {
+  name        = "AmazonS3FullAccessPolicy"
+  description = "Full Access to S3 and S3 Object Lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:*",
+          "s3-object-lambda:*"
         ],
         Resource = "*"
       }
@@ -44,7 +108,12 @@ resource "aws_iam_policy" "glue_access_policy" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "glue_access_attachment" {
+resource "aws_iam_role_policy_attachment" "glue_service_role_attachment" {
   role       = aws_iam_role.glue_access_role.name
-  policy_arn = aws_iam_policy.glue_access_policy.arn
+  policy_arn = aws_iam_policy.glue_service_role_policy.arn
+}
+
+resource "aws_iam_role_policy_attachment" "amazon_s3_full_access_attachment" {
+  role       = aws_iam_role.glue_access_role.name
+  policy_arn = aws_iam_policy.amazon_s3_full_access_policy.arn
 }
